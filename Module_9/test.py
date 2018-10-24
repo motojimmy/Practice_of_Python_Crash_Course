ee523cd71a68ee70fbfb40c576e3f25544e3dff0
motojimmy <fangjian.bupt@gmail.com>
2018-10-24 13:01:35 +0800

    2018-10-24 整合Module 8和9

---
+++ Module_9/test.py
@@ -0,0 +1,25 @@
+class Car():
+    """一次模拟汽车的简单尝试"""
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + " " + self.make + " " + self.model
+        return long_name
+
+class ElectricCar(Car):
+
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def descriptive_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kwh battery.")
+
+my_besla = ElectricCar('tesla', 'model s', 2016)
+print(my_besla.get_descriptive_name())
+my_besla.descriptive_battery()
