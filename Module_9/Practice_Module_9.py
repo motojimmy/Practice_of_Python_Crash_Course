4f8b59bb8cc0750f5bd0493ae33223c28060f500
MotoJimmy <fangjian.bupt@gmail.com>
2018-10-25 22:35:06 +0800

    Mac OS

--- Module_9/Practice_Module_9.py
+++ Module_9/Practice_Module_9.py
@@ -4,7 +4,7 @@ print("< 9-1 >")
 
 
 class Restaurant:
-
+    # 用于描述餐馆的类
     def __init__(self, restaurant_name, cuisine_type):
         self.name = restaurant_name
         self.type = cuisine_type
@@ -40,7 +40,7 @@ print("< 9-3 >")
 
 
 class User:
-
+    # 用于描述用户的类
     def __init__(self, first_name, last_name, gender, age):
         self.first_name = first_name
         self.last_name = last_name
@@ -149,7 +149,7 @@ print("< 9-6 >")
 
 
 class IceCreamStand(Restaurant):
-    
+    #  用于描述冰激凌甜品店的子类
     def __init__(self, restaurant_name, cuisine_type):
         super().__init__(restaurant_name, cuisine_type)
         self.flavors = ['super-sweet', 'medium-sweet',
@@ -176,6 +176,7 @@ print("\n**************************************************")
 print("< 9-7 >")
 
 class Admin(User):
+    # 用于描述管理员信息的类
     def __init__(self, first_name, last_name, gender, age):
         super(Admin, self).__init__(first_name, last_name, gender, age)
         self.privileges = ["can add post", "can delete post",
@@ -195,6 +196,7 @@ print("\n**************************************************")
 print("< 9-8 >")
 
 class Privileges:
+    # 用于描述权限的类
     def __init__(self):
         self.privileges = ["can add post", "can delete post",
                            "can ban user"]
@@ -205,6 +207,7 @@ class Privileges:
             print('- ' + privilege.title())
 
 class Admin(User):
+    # 用于描述管理员信息的类
     def __init__(self, first_name, last_name, gender, age):
         super(Admin, self).__init__(first_name, last_name, gender, age)
         self.privileges = Privileges()
@@ -220,7 +223,7 @@ print("< 9-9 >")
 
 
 class Car():
-
+# 用于描述汽车的类
     def __init__(self, make, model, year):
         self.make = make
         self.model = model
@@ -246,6 +249,7 @@ class Car():
 
 
 class Battery:
+# 用于描述电池的类
     def __init__(self, battery_size=70):
         self.battery_size = battery_size
 
@@ -270,7 +274,7 @@ class Battery:
 
 
 class ElectricCar(Car):
-
+# 用于描述电动汽车的类
     def __init__(self,make, model, year):
         super(ElectricCar, self).__init__(make, model, year)
         self.battery = Battery()
@@ -322,5 +326,54 @@ admin_03.privileges.show_privileges()
 print("\n**************************************************")
 print("< 9-13 >")
 
+from collections import OrderedDict
+
+dic_01 = OrderedDict()
+
+dic_01['print'] = 'print output value on screen'
+dic_01['exit'] = 'logout'
+dic_01['remove']= 'remove the data'
+
+for key, value in dic_01.items():
+    print(key + ": " + value)
 
 
+print("\n**************************************************")
+print("< 9-14 >")
+
+from random import randint
+class Die():
+# 用于模拟掷骰子的类,定义了 骰子面数 和 掷骰子次数
+    def __init__(self, sides, times):
+        self.sides = sides
+        self.times = times
+
+    def roll_die(self):
+        time = range(1, self.times + 1)
+        for n in time:
+            x = randint(1, self.sides)
+            print(x)
+        print("\n")
+
+shai_zi_01 = Die(6, 10)
+
+shai_zi_01.roll_die()
+
+
+
+shai_zi_02 = Die(10, 10)
+
+shai_zi_02.roll_die()
+
+
+shai_zi_03 = Die(20, 10)
+
+shai_zi_03.roll_die()
+
+
+
+print("\n**************************************************")
+print("< 9-15 >")
+
+print('Visit the website http://pymotw.com and find more '
+      'information about the Standard Library. ')
