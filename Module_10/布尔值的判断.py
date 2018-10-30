9a46598de45a9fa61c3b7a0a39c1dec70e3c0e57
MotoJimmy <fangjian.bupt@gmail.com>
2018-10-30 23:07:00 +0800

    Mac OS

---
+++ Module_10/布尔值的判断.py
@@ -0,0 +1,28 @@
+def test_01():
+    stack = []
+    return not stack
+
+def test_02():
+    stack = [1,2,3]
+    return not stack
+
+def test_03():
+    stack = [1,2,3]
+    return stack
+
+
+s = test_01()
+print(s)
+
+s = test_02()
+print(s)
+
+s = test_03()
+print(s)
+
+print(not 0)
+print(not [])
+print(not 2)
+print(not [2,3,4])
+
+
